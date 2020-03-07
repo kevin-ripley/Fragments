280cacd99c966581eaa5de7da372762f593aa11a
Kevin Ripley <kevin.r.ripley.kr@gmail.com>
2020-03-06 17:55:43 -0700

    Fragment 1 section done and complete

--- app/src/main/java/com/example/android/fragmentexample1/SimpleFragment.kt
+++ app/src/main/java/com/example/android/fragmentexample1/SimpleFragment.kt
@@ -1,4 +1,4 @@
-package com.example.android.fragmentexample1
+package com.example.android.fragmentexample1;
 
 
 import android.os.Bundle
@@ -6,17 +6,38 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioGroup
+import android.widget.TextView
+
 
 /**
  * A simple [Fragment] subclass.
  */
 class SimpleFragment : Fragment() {
+// Inflate the layout for this fragment.
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_simple, container, false)
+    val YES: Int = 0
+    val NO: Int = 1
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        val rootView = inflater.inflate(R.layout.fragment_simple, container, false)
+        val radioGroup: RadioGroup = rootView.findViewById(R.id.radio_group)
+
+        radioGroup.setOnCheckedChangeListener { group, checkedId ->
+            val radioButton = radioGroup.findViewById<View>(checkedId)
+            val index = radioGroup.indexOfChild(radioButton)
+            val textView = rootView.findViewById<TextView>(R.id.fragment_header)
+            when (index) {
+                YES -> textView.setText(R.string.yes_message)
+                NO -> textView.setText(R.string.no_message)
+                else -> {
+                }
+            }
+        }
+
+// Return the View for the fragment's UI.
+        return rootView
     }
-
 
 }
